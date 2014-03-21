aa5d15c443612ef8bb10220efae7e733bb059b5b
Zhen Jiang <jiang.zhenhang@gmail.com>
2014-03-21 18:47:26 -0400

    testing changer

--- src/hand_sensing.c
+++ src/hand_sensing.c
@@ -1,6 +1,13 @@
+#include <stdio.h>
+#include "charger.h"
 
 void handSensing(){
+    charger_t *charger = malloc(sizeof(charger_t));
+    charger_init(charger);
     while(1){
-        printf("hello");
+        charger_run(charger);
+        printf("x time: %d; y time: %d; z time: %d",
+                charger->xTime, charger->yTime, charge->zTime);
+        charger->newDataFlag = 0;
     }
 }
