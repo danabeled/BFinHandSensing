e651f56d8ad2f2528fd949766383029f59c08182
danabeled <danabeled@gmail.com>
2014-03-21 17:59:43 -0400

    Creation of charger class alpha version

---
+++ inc/charger.h
@@ -0,0 +1,61 @@
+ /******************************************************************************
+ *@file: charger.h
+ *
+ *@brief: 
+ *  - This is a the declarations for our charger class
+ *    
+ *    
+ *
+ * Target:   TLL6527v1-0      
+ * Compiler: VDSP++     Output format: VDSP++ "*.dxe"
+ *
+ * @author:  Daniel Abel
+ * @created: 03/21/14
+ *
+ *******************************************************************************/
+  
+#ifndef CHARGER_H_
+#define CHARGER_H_
+
+
+
+ /***************** Definitions  ********************************************/
+/* Any definitions that can be seen outside  */
+
+
+/***************** Data Type Definitions ************************************/
+
+/** template attributes
+ */
+typedef struct {
+	unsigned int xTime; /** time for the x plate to charge */
+	unsigned int yTime; /** time for the y plate to charge */
+	unsigned int zTime; /** time for the z plate to charge */
+	unsigned int numPlatesCharged; /** number of plates curerntly charged **/
+	unsigned short newDataFlag; /** allows readers to know there is new position data **/
+} charger_t;
+
+/***************** Method Prototypes ****************************************/
+
+/** Initialization for charger, inits interrupts for x, y, z. Inits timer.
+ *
+ * @param pThis  pointer to own object
+ *
+ * @return Zero on success, negative otherwise 
+ */
+int charger_init(charger_t *pThis);
+
+/** starts the chargers polling loop **/
+int charger_run(charger_t *pThis);
+
+/** set GPIO bits to output and reintialize timer **/
+int charger_discharge(charger_t *pThis);
+
+/** set GPIO bits to input, start timer, poll until 3 plates
+	are all charged **/
+int charger_charge(charger_t *pThis);
+
+
+
+
+ #endif
---
+++ src/charger.c
@@ -0,0 +1,58 @@
+/******************************************************************************
+ *@file: charger.c
+ *
+ *@brief: 
+ *  - Implementation of charging code to sense time to charge sensors
+ *
+ * Target:   TLL6527v1-0      
+ * Compiler: VDSP++     Output format: VDSP++ "*.dxe"
+ *
+ * @author:  Daniel Abel
+ * @created: 03/21/14
+ *
+ *******************************************************************************/
+
+#include <charger.h>
+
+/***************** Private Defines *****************************/
+#define CHARGER_G_BITS 0x1C
+
+ /*****************  Methods Implementations **********************************/
+
+/** Initialization for template
+ *
+ * @param pThis  pointer to own object
+ *
+ * @return Zero on success, negative otherwise 
+ */
+int charger_init(template_t *pThis) {
+	
+	/* 	initialize Port G's 2-4 bits direction, and clear
+		the bits. Set x, y, z, and number of plates
+		charged to zero.  */
+	*pPORTGIO_DIR &= ~(CHARGER_G_BITS);
+	*pPORTGIO_CLEAR |= CHARGER_G_BITS;
+	pThis->xTime = 0;
+	pThis->yTime = 0;
+	pThis->zTime = 0;
+	pThis->numPlatesCharged = 0;
+	pThis->newDataFlag = 0;
+
+	/* 	call timer's initialization function 		*/
+	timer_init();
+}
+
+/** Starts the chargers polling loop 
+ *
+ * @param pThis  pointer to own object
+ *
+ * @return Zero on success, negative otherwise 
+ * */
+int charger_run(charger_t *pThis);
+
+/** set GPIO bits to output and reintialize timer **/
+int charger_discharge(charger_t *pThis);
+
+/** set GPIO bits to input, start timer, poll until 3 plates
+	are all charged **/
+int charger_charge(charger_t *pThis);
